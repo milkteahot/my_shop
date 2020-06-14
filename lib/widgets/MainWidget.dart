e264aafc9867777f916e644ce9be9a7b31d2861d
milkteahot <greenegoode11@gmail.com>
2020-06-14 18:10:47 +0900

    TabBar 추가

--- lib/widgets/MainWidget.dart
+++ lib/widgets/MainWidget.dart
@@ -8,11 +8,46 @@ class MainWidget extends StatefulWidget {
 class _MainWidgetState extends State<MainWidget> {
   @override
   Widget build(BuildContext context) {
-    return Scaffold(
-      body: SafeArea(
-        child: Text(
-          '메인 위젯입니다',
+    return DefaultTabController(
+      length: 3,
+      child: Scaffold(
+        appBar: AppBar(
+          automaticallyImplyLeading: false,
+          flexibleSpace: SafeArea(
+            child: TabBar(
+              tabs: [
+                Tab(
+                  icon: Icon(Icons.shopping_basket),
+                  text: 'Item List',
+                ),
+                Tab(
+                  icon: Icon(Icons.shopping_cart),
+                  text: 'Cart',
+                ),
+                Tab(
+                  icon: Icon(Icons.person),
+                  text: 'Profile',
+                ),
+              ]
+            ),
+          ),
+        ),
+
+      body: TabBarView(
+        children: <Widget>[
+          Text(
+          '아이템 리스트입니다',
           style: TextStyle(fontSize: 15.0),
+          ),
+          Text(
+            '카트입니다',
+            style: TextStyle(fontSize: 15.0),
+          ),
+          Text(
+            '프로필입니다',
+            style: TextStyle(fontSize: 15.0),
+          ),
+        ]
         ),
       ),
     );
